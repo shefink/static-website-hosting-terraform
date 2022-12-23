f7db5f8ca39edba03a838abd42e61fc9a32e3726
Ashiq Ummathoor <ashiqit93@gmail.com>
2022-12-23 18:52:05 +0530

    Update project.tf

--- project.tf
+++ project.tf
@@ -19,3 +19,6 @@ resource "aws_s3_bucket_acl" "example_bucket_acl" {
   bucket = aws_s3_bucket.s3.id
   acl    = "public-read"
 }
+output "s3-website-url" {
+  value = aws_s3_bucket_website_configuration.website.id
+}
