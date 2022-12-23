a5f453cb0c4890aaab71758eefb36aeadca933db
Ashiq Ummathoor <ashiqit93@gmail.com>
2022-12-23 18:33:37 +0530

    terraform-pro

---
+++ project.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  version = "~>3.0"
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "s3" {
+  bucket = "12-23-2002"
+}
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.s3.id
+  policy = file("policy.json")
+}
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.s3.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.s3.id
+  acl    = "public-read"
+}
